f46edfcea5e2c63041c2a841b88b30462778f7bd
Rohan Dhodare <87656778+RohanDhodare@users.noreply.github.com>
2025-10-20 12:43:05 +0530

    Added Spring boot started project
    It has Java version as 25 and spring boot version as 3.5.6 and all basic and necessary dependencies

---
+++ commonJar/commonJar/src/main/java/com/rohanExercises/commonJar/CommonJarApplication.java
@@ -0,0 +1,13 @@
+package com.rohanExercises.commonJar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonJarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonJarApplication.class, args);
+	}
+
+}
---
+++ commonJar/commonJar/src/test/java/com/rohanExercises/commonJar/CommonJarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohanExercises.commonJar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonJarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
